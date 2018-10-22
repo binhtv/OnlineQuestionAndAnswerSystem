acd8f4f6ac2dbf38b920a08b36ead7319c2e4e1a
dawitkwm <dawitkahsay@gmail.com>
2018-10-22 16:53:49 -0500

    added .settings

--- src/main/java/com/codnel/controller/HomeController.java
+++ src/main/java/com/codnel/controller/HomeController.java
@@ -1,5 +1,14 @@
 package com.codnel.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class HomeController {
+	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
+	public String showHome() {
+		return "welcome";
+	}
 
 }
